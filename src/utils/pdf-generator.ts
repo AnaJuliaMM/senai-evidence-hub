e17ac54111acef0abd1403a2d5272371bee83795
Ana Julia M Martins <ana.martins_2004@outlook.com>
2026-03-29 17:28:04 -0300

    feat(pdf): use company default mask

--- src/utils/pdf-generator.ts
+++ src/utils/pdf-generator.ts
@@ -1,4 +1,5 @@
 import jsPDF from "jspdf";
+import {template_base64} from "./template_base64"
 
 interface AtividadeData {
   nome: string;
@@ -18,175 +19,133 @@ function loadImage(url: string): Promise<HTMLImageElement> {
   });
 }
 
-export async function gerarPdfAtividade(atividade: AtividadeData): Promise<Blob> {
+export async function gerarPdfAtividade(
+  atividade: AtividadeData
+): Promise<Blob> {
   const doc = new jsPDF("p", "mm", "a4");
 
+
+  // Parametros de configuracao ---------------------
   const pageWidth = doc.internal.pageSize.getWidth();
   const pageHeight = doc.internal.pageSize.getHeight();
+  const marginLeft = 20;
+  const contentWidth = pageWidth - marginLeft * 2;
+  const lineHeight = 6
+  let marginTop = 40;
 
-  const margin = 20;
-  const contentWidth = pageWidth - margin * 2;
 
-  let y = 20;
+  // Posicionar a mascara oficial ---------------------
+  doc.addImage(template_base64, "PNG", 0, 0, 210, 297);
 
-  /* ================================
-     HEADER
-  ================================= */
 
-  doc.setFillColor(0, 90, 156);
-  doc.rect(0, 0, pageWidth, 35, "F");
-
-  doc.setFillColor(200, 30, 30);
-  doc.rect(0, 35, pageWidth, 4, "F");
-
-  doc.setTextColor(255, 255, 255);
-
+  // Formatar titulo ---------------------
   doc.setFont("helvetica", "bold");
-  doc.setFontSize(20);
+  doc.setFontSize(16);
+  doc.setTextColor(0, 0, 0); 
+  doc.text(
+    "RELATÓRIO DE EVIDÊNCIA", 
+    pageWidth / 2, 
+    marginTop, 
+    { align: "center" }
+  );
+  marginTop += lineHeight*2;
 
-  doc.text("RELATÓRIO DE EVIDÊNCIA", pageWidth / 2, 18, { align: "center" });
 
+  // Formatar campos do formulario ---------------------
+  const formatFormsFields = (label: string, value: string) => {
+      // Formatar e posicionar o campo
+      doc.setFontSize(12);
+      doc.setFont("helvetica", "bold");
+      doc.text(`${label}: `, marginLeft, marginTop);
+  
+      // Formatar e posicionar o valor
+      const labelWidth = doc.getTextWidth(`${label}: `);
+      const valueStartMargin = marginLeft + labelWidth;
+      const availableWidth = contentWidth - labelWidth;
+      const valueLines = doc.splitTextToSize(value, availableWidth);
+  
+      doc.setFontSize(12);
+      doc.setFont("helvetica", "normal");
+      doc.text(valueLines, valueStartMargin, marginTop);
+  
+      // Atualizar a margem 
+      marginTop += (valueLines.length * lineHeight);
+    };
+
+  // titulo
+  // tipo 
+  formatFormsFields("Responsável", atividade.nome);
+  // empresa parceira (se tiver)
+  formatFormsFields("Local", atividade.local);
+  formatFormsFields(
+    "Data", 
+    new Date(atividade.data + "T00:00:00").toLocaleDateString("pt-BR")
+  );
+  
+
+  // Formatar secao descricao ---------------------
+  marginTop += lineHeight*2
+  doc.setFont("helvetica", "bold");
   doc.setFontSize(14);
-  doc.text("SENAI", pageWidth / 2, 28, { align: "center" });
-
-  y = 50;
-
-  /* ================================
-     CARD CONTAINER
-  ================================= */
-
-  doc.setDrawColor(220, 220, 220);
-  doc.setFillColor(255, 255, 255);
-
-  doc.roundedRect(
-    margin - 5,
-    y - 10,
-    contentWidth + 10,
-    120,
-    4,
-    4,
-    "FD"
+  doc.text(
+    "DESCRIÇÃO", 
+    pageWidth / 2, 
+    marginTop, 
+    { align: "center" }
   );
 
-  /* ================================
-     FIELD FUNCTION
-  ================================= */
+  marginTop += lineHeight*2
+  doc.setFont("helvetica", "normal");
+  doc.setFontSize(12);
+  const descriptionLines = doc.splitTextToSize(atividade.descricao, contentWidth);
+  doc.text(descriptionLines, marginLeft, marginTop);
 
-  const addField = (label: string, value: string) => {
-    doc.setFontSize(9);
-    doc.setTextColor(120, 120, 120);
-    doc.setFont("helvetica", "bold");
 
-    doc.text(label.toUpperCase(), margin, y);
-
-    y += 5;
-
-    doc.setFontSize(12);
-    doc.setTextColor(40, 40, 40);
-    doc.setFont("helvetica", "normal");
-
-    const lines = doc.splitTextToSize(value, contentWidth);
-
-    doc.text(lines, margin, y);
-
-    y += lines.length * 6 + 6;
-  };
-
-  /* ================================
-     DATA FORMAT
-  ================================= */
-
-  const dataFormatada = new Date(
-    atividade.data + "T00:00:00"
-  ).toLocaleDateString("pt-BR");
-
-  /* ================================
-     CONTENT
-  ================================= */
-
-  addField("Responsável", atividade.nome);
-
-  addField("Descrição da atividade", atividade.descricao);
-
-  addField("Data", dataFormatada);
-
-  addField("Local", atividade.local);
-
-  /* ================================
-     PHOTOS
-  ================================= */
-
+  // Formatar secao evidencias ---------------------
   if (atividade.fotosUrls.length > 0) {
-    y += 6;
-
-    doc.setFontSize(12);
+    marginTop += lineHeight*2
     doc.setFont("helvetica", "bold");
-    doc.setTextColor(0, 90, 156);
+    doc.setFontSize(14);
+    doc.text(
+      "EVIDÊNCIAS", 
+      pageWidth / 2, 
+      marginTop, 
+      { align: "center" }
+    );
+    marginTop += lineHeight*2
 
-    doc.text("REGISTROS FOTOGRÁFICOS", margin, y);
+    // const imgWidth = (contentWidth - 10) / 2;
+    // let x = marginLeft;
+    // for (let i = 0; i < atividade.fotosUrls.length; i++) {
+    //   const url = atividade.fotosUrls[i];
 
-    y += 10;
+    //   try {
+    //     const img = await loadImage(url);
 
-    const imgWidth = (contentWidth - 10) / 2;
-    let x = margin;
+    //     const ratio = img.height / img.width;
+    //     const imgHeight = imgWidth * ratio;
 
-    for (let i = 0; i < atividade.fotosUrls.length; i++) {
-      const url = atividade.fotosUrls[i];
+    //     if (y + imgHeight > pageHeight - 30) {
+    //       doc.addPage();
+    //       y = 20;
+    //     }
 
-      try {
-        const img = await loadImage(url);
+    //     doc.addImage(img, "JPEG", x, y, imgWidth, imgHeight);
 
-        const ratio = img.height / img.width;
-        const imgHeight = imgWidth * ratio;
-
-        if (y + imgHeight > pageHeight - 30) {
-          doc.addPage();
-          y = 20;
-        }
-
-        doc.addImage(img, "JPEG", x, y, imgWidth, imgHeight);
-
-        if (x === margin) {
-          x = margin + imgWidth + 10;
-        } else {
-          x = margin;
-          y += imgHeight + 10;
-        }
-      } catch {
-        doc.setTextColor(150, 150, 150);
-        doc.text("[imagem não disponível]", x, y);
-      }
-    }
+    //     if (x === margin) {
+    //       x = margin + imgWidth + 10;
+    //     } else {
+    //       x = margin;
+    //       y += imgHeight + 10;
+    //     }
+    //   } catch {
+    //     doc.setTextColor(150, 150, 150);
+    //     doc.text("[imagem não disponível]", x, y);
+    //   }
+    // }
+    
   }
 
-  /* ================================
-     FOOTER
-  ================================= */
-
-  const pageCount = doc.getNumberOfPages();
-
-  for (let i = 1; i <= pageCount; i++) {
-    doc.setPage(i);
-
-    doc.setDrawColor(220, 220, 220);
-    doc.line(margin, pageHeight - 18, pageWidth - margin, pageHeight - 18);
-
-    doc.setFontSize(8);
-    doc.setTextColor(140, 140, 140);
-
-    doc.text(
-      "SENAI • Registro de Evidências de Atividades",
-      margin,
-      pageHeight - 10
-    );
-
-    doc.text(
-      `Página ${i} de ${pageCount}`,
-      pageWidth - margin,
-      pageHeight - 10,
-      { align: "right" }
-    );
-  }
 
   return doc.output("blob");
 }
